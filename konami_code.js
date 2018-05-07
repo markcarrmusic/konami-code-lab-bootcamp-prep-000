a38babe66f6805aa36ed7319722f88eda4a11a5b
Mark Carr <markcarrmusic@gmail.com>
2018-05-07 20:34:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,21 @@
-const code = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+let index = 0;
 
 function init() {
-  let index = 0;
-  document.body.addEventListener('keydown', function(e) {
-    const key = e.key;
+  document.body.addEventListener('keydown', function (e) {
+  const key = parseInt(e.detail || e.which);
 
-    if (key === code[index]) { 
+  if (key === code[index]) {
+    index++;
 
-     if (index === code.length-1) {
-        alert('Hurray!');
+    if (index === code.length) {
+      alert("Congratulations!");
 
-        index = 0;
-      }
-    }
-    else {
       index = 0;
     }
+  } else {
+    index = 0;
+  }
   });
 }
