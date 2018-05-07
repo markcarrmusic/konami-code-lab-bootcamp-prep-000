ccd4915813d0e451973ceb9bc1b551e805ae974b
Mark Carr <markcarrmusic@gmail.com>
2018-05-07 20:19:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,7 @@
 const code = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
 
-let index = 0;
-
 function init() {
+  let index = 0;
   document.body.addEventListener('keydown', function(e) {
     let key = e.key;
 
